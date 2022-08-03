aca048d196d70081b58888fdd33a167ecbcdae37
kaioruan <kaio.ruan@souunit.com.br>
2022-08-03 14:02:29 -0300

    Feat: create route PUT edit talker

--- Middlewares/LoginValidation.js
+++ Middlewares/LoginValidation.js
@@ -63,11 +63,11 @@ const talkValidation = (req, res, next) => {
 
 const rateValidation = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk.rate) {
-    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  }
   if (talk.rate < 1 || talk.rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  if (!talk.rate) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
   next();
 };
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const TALKER_JSON = 'talker.json';
 
 // não remova esse endpoint,  para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -20,21 +21,21 @@ app.get('/', (_request, response) => {
 
 app.get('/talker/search', tokenValidation, (req, res) => {
   const { q } = req.query;
-  const response = fs.readFileSync('talker.json', 'utf8');
+  const response = fs.readFileSync(TALKER_JSON, 'utf8');
   const result = JSON.parse(response);
   const filteredTalker = result.filter((r) => r.name.includes(q));
   res.status(200).json(filteredTalker);
 });
 
 app.get('/talker', (_req, res) => {
-  const response = fs.readFileSync('talker.json', 'utf8');
+  const response = fs.readFileSync(TALKER_JSON, 'utf8');
   const result = JSON.parse(response);
   res.status(200).json(result);
 });
 
 app.get('/talker/:id', (req, res) => {
   const { id } = req.params;
-  const response = fs.readFileSync('talker.json', 'utf8');
+  const response = fs.readFileSync(TALKER_JSON, 'utf8');
   const result = JSON.parse(response);
   const talkerId = result.find((r) => r.id === Number(id));
   if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
@@ -45,9 +46,29 @@ app.post('/login', emailValidation, passwordValidation, generateToken);
 
 app.post('/talker', tokenValidation, nameAgeValidation, talkValidation, rateValidation, newTalker);
 
+app.put('/talker/:id', 
+tokenValidation, nameAgeValidation, talkValidation, rateValidation, (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const response = fs.readFileSync(TALKER_JSON, 'utf8');
+  const result = JSON.parse(response);
+  const filterTalker = result.filter((r) => r.id !== Number(id));
+  console.log(result);
+  const editTalker = {
+    id: Number(id),
+    name,
+    age,
+    talk,
+  };
+  filterTalker.push(editTalker);
+  fs.writeFileSync('./talker.json', JSON.stringify(filterTalker));
+
+  res.status(200).send(editTalker);
+});
+
 app.delete('/talker/:id', tokenValidation, async (req, res) => {
   const { id } = req.params;
-  const response = fs.readFileSync('talker.json', 'utf8');
+  const response = fs.readFileSync(TALKER_JSON, 'utf8');
   const result = JSON.parse(response);
   const newResult = result.filter((r) => r.id !== Number(id));
   fs.writeFileSync('./talker.json', JSON.stringify(newResult));
